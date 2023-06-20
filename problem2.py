bee59226bb25c0bce12947534bd20cbcbe00477c
floraohlsen <136815371+floraohlsen@users.noreply.github.com>
2023-06-20 14:26:49 +0200

    solution for issue 2 (#5)
    Co-authored-by: “Flora <flora.ohlsen@zalando.de>

---
+++ problem2.py
@@ -0,0 +1,11 @@
+def simpleArraySum(ar):
+    # Initialize a variable to store the sum
+    totalsum = 0
+
+    # Iterate over each element in the array
+    for num in ar:
+        # Add the current element to the sum
+        totalsum += num
+
+    # Return the sum
+    return totalsum
